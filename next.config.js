afa15b40ca9045722cea97316f0abef52af6a1f8
Justharoon <156121090+Justharoon@users.noreply.github.com>
2025-02-10 17:12:44 +0000

    Remove deprecated experimental options from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
-  },
-  // Disable server components since we're doing static export
-  experimental: {
-    appDir: true,
   },
 }
 
